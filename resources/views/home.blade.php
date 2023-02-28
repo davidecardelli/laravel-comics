2c7d044230344c9e036619f22b3778267290cce9
davidecardelli <davide.cardelli.94@gmail.com>
2023-02-28 17:34:36 +0100

    fixed

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,11 +1,11 @@
 @extends('template.main')
 
 @section('content')
-<main>
-    <div class="container main-content">
-        @foreach ($comics as $comic)
-        @include('includes.card')
-        @endforeach
-    </div>
-<main>
+    <main>
+        <div class="container main-content">
+            @foreach ($comics as $comic)
+                @include('includes.card')
+            @endforeach
+        </div>
+    </main>
 @endsection
